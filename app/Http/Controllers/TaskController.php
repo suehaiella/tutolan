82e2d103eefd0eae59b0211e02f7f68d88f34973
suehaiella <suehaiell1@gmail.com>
2019-02-25 17:54:17 +0800

    first

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   //1. query database for collecting data task 
+        //"select * from Task"
+        $tasks = \App\Task::all();
+        //dd($tasks);
+
+        //2. return database to view 
+        //view refer to folder res=ourece/iew
+        return view('task.index', compact('tasks'));
+        //dd($task);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+
+					  <tr>
+					    <th>Tasks</th>
+					    <th>Pic</th>
+					    <th>Email</th>
+					     <th>Phone</th>
+					  </tr>
+					  @foreach($tasks as $task)
+					  <tr>
+					    <td>{{ $task->name }}</td>
+					    <td>{{ $task->pic }}</td>
+					    <td>{{ $task->email }}</td>
+					    <td>{{ $task->phone_no }}</td>
+					  </tr>
+					  @endforeach
+
+					</table>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
